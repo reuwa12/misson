f655a0a3904bfeb3556e3408bf7fa75e95ea6f79
USER <mon001024@naver.com>
2025-06-27 18:36:45 +0900

    seven

---
+++ src/one.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class one {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double number1;
+        double number2;
+        double result = 0;
+        boolean ok = true;
+        System.out.println("첫번째 숫자를 입력 해주세요");
+        System.out.println("+,- 중 골라주세요);
+
+                if
+        int choice = scanner.nextInt();
+
+
+        switch (choice) {
+            case 1 : System.out.println("+");
+                break;
+
+            case 2 : System.out.println();
+                break;
+
+            case 3 :System.out.println("-");
+                break;
+
+            default : System.out.println("다시 입력 해주세요");
+
+
+        }
+    }
+}
